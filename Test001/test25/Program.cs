25a162eab4363189c77fff88f0df23d7d6a0af40
geolog1990 <trece.espada@gmail.com>
2020-07-27 01:08:08 +0500

    Задача 25 сделана

--- Test001/test25/Program.cs
+++ Test001/test25/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace test25
 {
@@ -10,19 +12,36 @@ namespace test25
             char result;
             StreamReader streamReader = new StreamReader("input.txt");
             string s = streamReader.ReadToEnd();
+            try
+            {
+                a = s.First();
+                b = s.Last();
+                
+                if (a < b)
+                {
+                    result = '<';
+                }
+                else if (a > b)
+                {
+                    result = '>';
+                }
+                else
+                {
+                    result = '=';
+                }
 
-            // if (a < b)
-            // {
-            //     result = "<";
-            // }
-            // else if (a > b)
-            // {
-            //     result = ">";
-            // }
-            // else
-            // {
-            //     result = "=";
-            // }
+                using (StreamWriter streamWriter = new StreamWriter("output.txt"))    
+                {
+                    streamWriter.Write(result);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+           
         }
     }
 }
