aba85316699c6677ba231827cc3df9b1679149f2
Bulat Sharipov <trece.espada@gmail.com>
2020-08-04 01:03:26 +0500

    Решение задачи 903

--- Test001/test903/Program.cs
+++ Test001/test903/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace test903
+﻿using System;
+using System.IO;
+
+namespace test903
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            StreamReader streamReader = new StreamReader("input.txt");
+            string s = streamReader.ReadLine();
+            int a = Int32.Parse(s);
+            a = a + 1;
+            using (StreamWriter streamWriter = new StreamWriter("output.txt"))
+            {  
+               streamWriter.Write(a); 
+            }
         }
     }
 }
