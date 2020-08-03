db7bbb6aa10f3000e12dcf512ad0ab10d721eeec
Bulat Sharipov <trece.espada@gmail.com>
2020-08-04 00:39:39 +0500

    Revert "Revert " Решение задачи 108""
    This reverts commit 7e39eefdb573e93274680a6582fb0b2bcb8dd943.

---
+++ Test001/test108/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace test108
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            StreamReader streamReader = new StreamReader("input.txt");
+            string s = streamReader.ReadLine();
+            using (StreamWriter streamWriter = new StreamWriter("output.txt"))
+            {
+                streamWriter.Write(s);
+            }
+        }
+    }
+}
