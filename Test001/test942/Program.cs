c8e2c9c94b11c38724d8f34bb9167149a66656b0
Bulat Sharipov <trece.espada@gmail.com>
2020-08-05 01:35:19 +0500

    942

---
+++ Test001/test942/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace test942
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            StreamReader streamReader = new StreamReader("input.txt");
+            string N = streamReader.ReadLine();
+        }
+    }
+}
