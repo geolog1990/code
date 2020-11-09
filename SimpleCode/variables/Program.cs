504c001e4b87af12d8c6f90f50bc488d29c5f27f
Bulat Sharipov <trece.espada@gmail.com>
2020-11-09 20:48:12 +0500

    Update Program.cs

--- SimpleCode/variables/Program.cs
+++ SimpleCode/variables/Program.cs
@@ -7,13 +7,17 @@ namespace variables
         static void Main(string[] args)
         {
             int a, b;
+            bool var = false;
+            bool var1  = true;
+            string c = "#fdf";
 
             a = 5;
 
             b = 99;
 
-            Console.WriteLine(a);
-            Console.WriteLine(b);
+           Console.WriteLine(a);
+            a = 89;
+           Console.WriteLine(a);
         }
     }
 }
