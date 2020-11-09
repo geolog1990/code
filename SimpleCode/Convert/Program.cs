c032380b665b45564a708f2d9cfa508e5e8ba71c
Bulat Sharipov <trece.espada@gmail.com>
2020-11-09 22:01:50 +0500

    Update Program.cs

--- SimpleCode/Convert/Program.cs
+++ SimpleCode/Convert/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
+//конвертация строки
+
 namespace Convert
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string a = "3";
+            
+            string b = "5";
+            Console.WriteLine(a + b);
+            Convert.    
         }
     }
 }
