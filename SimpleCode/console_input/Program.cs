9d81b3e071db015e4de442523a28c066831da563
Bulat Sharipov <trece.espada@gmail.com>
2020-11-09 20:50:35 +0500

    create new project console input

---
+++ SimpleCode/console_input/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
