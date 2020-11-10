88822f52cfa38a6f4f2339485e36188938d11f82
Bulat Sharipov <trece.espada@gmail.com>
2020-11-10 13:51:12 +0500

    Create new project "Parse"

---
+++ SimpleCode/Try_Parse/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+// конвертация строки
+// parse и tryparse
+
+namespace Try_Parse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "5";
+
+            int a;
+
+            bool result = int.TryParse(str, out a);
+
+            if (result)
+            {
+                Console.WriteLine("Конвертация успешна" + result);
+            }
+            else
+            {
+                Console.WriteLine("Не удалось" + result);
+            }
+            /*try
+            {
+                double a = double.Parse(str);
+                Console.WriteLine("Успешная конвертация, а = " + a);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Ошибка при конвертации");
+            }
+            /*
+
+
+            /*
+             * NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            { 
+                NumberDecimalSeparator = ".",
+            };
+            
+            double a = double.Parse(str, numberFormatInfo);
+            */
+        }
+    }
+}
