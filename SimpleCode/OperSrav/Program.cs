22d9b643f97fee37312025accbfcc7b6173fb8b2
Bulat Sharipov <trece.espada@gmail.com>
2020-11-12 00:35:57 +0500

    create

---
+++ SimpleCode/OperSrav/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OperSrav
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            int b = 5;
+            //bool result = a == b;
+            Console.WriteLine(a >= b);
+        }
+    }
+}
