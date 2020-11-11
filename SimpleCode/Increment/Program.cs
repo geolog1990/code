101c641974cb8cd663f6198b028601e79bab91b3
Bulat Sharipov <trece.espada@gmail.com>
2020-11-11 13:46:53 +0500

    Create new project

---
+++ SimpleCode/Increment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+//инкремент и декремент. Унарные операторы
+
+namespace Increment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
