226fb0876a42fdece9e580a4df4963e63e08a229
Bulat Sharipov <trece.espada@gmail.com>
2020-11-16 00:42:07 +0500

    create new project

---
+++ SimpleCode/switch/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tswitch
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+            int a = int.Parse(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("ввели число 1");
+                    break;
+                case 2:
+                    Console.WriteLine("ввели число 2");
+                    break;
+            }
+        }
+}
+}
