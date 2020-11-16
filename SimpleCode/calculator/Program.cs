fffd514460cef7091e35a945af1bd40aa4c666c1
Bulat Sharipov <trece.espada@gmail.com>
2020-11-17 00:11:47 +0500

    create calculator

---
+++ SimpleCode/calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первую цифру");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите вторую цифру");
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Какую операцию мне произвести?");
+            string str = Console.ReadLine();
+            switch (str)
+            {
+                case "+":
+                    Console.WriteLine(firstNumber + "+" + secondNumber + "=" + (firstNumber + secondNumber));
+                    break;
+                case "-":
+                    Console.WriteLine(firstNumber + "-" + secondNumber + "=" + (firstNumber - secondNumber));
+                    break;
+                case "*":
+                    Console.WriteLine(firstNumber + "*" + secondNumber + "=" + (firstNumber * secondNumber));
+                    break;
+                case "/":
+                    Console.WriteLine(firstNumber + "/" + secondNumber + "=" + (firstNumber / secondNumber));
+                    break;
+                default:
+                    Console.WriteLine("Этого я пока не умею");
+                    break;
+            }
+        }
+    }
+}
