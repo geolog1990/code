3b8b11b93609718e70fd01bb2adbaa97ce792e5b
Bulat Sharipov <trece.espada@gmail.com>
2020-11-17 23:49:19 +0500

    create new project

---
+++ SimpleCode/cycle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace cycle
+{
+    /*
+     * Цикл while
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            while (true)
+            {
+                count++;
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
