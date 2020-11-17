2272adb1bc83a1694bfd0e9ad6091cd1dc74b083
Bulat Sharipov <trece.espada@gmail.com>
2020-11-18 01:13:11 +0500

    create new project

---
+++ SimpleCode/CycleFor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CycleFor
+{
+    /*
+     * Цикл for 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
