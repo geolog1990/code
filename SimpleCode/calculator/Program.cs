0cc1c4a459161c033d798914f210365c74f238a2
Bulat Sharipov <trece.espada@gmail.com>
2020-11-17 22:56:57 +0500

    Update Program.cs

--- SimpleCode/calculator/Program.cs
+++ SimpleCode/calculator/Program.cs
@@ -6,61 +6,65 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите первую цифру");
-            double firstNumber = double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите вторую цифру");
-            double secondNumber = double.Parse(Console.ReadLine());
-            Console.WriteLine("Какую операцию мне произвести? : '+' '-' '*' '/'");
-            string action = Console.ReadLine();
-            
-            switch (action)
+            while (true)
             {
-                case "+":
-                    Console.WriteLine(firstNumber + "+" + secondNumber + "=" + (firstNumber + secondNumber));
-                    break;
-                case "-":
-                    Console.WriteLine(firstNumber + "-" + secondNumber + "=" + (firstNumber - secondNumber));
-                    break;
-                case "*":
-                    Console.WriteLine(firstNumber + "*" + secondNumber + "=" + (firstNumber * secondNumber));
-                    break;
-                case "/":
+                Console.WriteLine("Введите первую цифру");
+                double firstNumber = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите вторую цифру");
+                double secondNumber = double.Parse(Console.ReadLine());
+                Console.WriteLine("Какую операцию мне произвести? : '+' '-' '*' '/'");
+                string action = Console.ReadLine();
+
+                switch (action)
+                {
+                    case "+":
+                        Console.WriteLine(firstNumber + "+" + secondNumber + "=" + (firstNumber + secondNumber));
+                        break;
+                    case "-":
+                        Console.WriteLine(firstNumber + "-" + secondNumber + "=" + (firstNumber - secondNumber));
+                        break;
+                    case "*":
+                        Console.WriteLine(firstNumber + "*" + secondNumber + "=" + (firstNumber * secondNumber));
+                        break;
+                    case "/":
+                        if (secondNumber == 0)
+                            Console.WriteLine(0);
+                        else
+                            Console.WriteLine(firstNumber + "/" + secondNumber + "=" + (firstNumber / secondNumber));
+                        break;
+                    default:
+                        Console.WriteLine("Этого я пока не умею");
+                        break;
+                }
+
+                /*
+                if (action == "+")
+                {
+                    Console.WriteLine(firstNumber + action + secondNumber + "=" + (firstNumber + secondNumber));
+                }
+                else if (action == "-")
+                {
+                    Console.WriteLine(firstNumber + action + secondNumber + "=" + (firstNumber - secondNumber));
+                }
+                else if (action == "*")
+                {
+                    Console.WriteLine(firstNumber + action + secondNumber + "=" + (firstNumber * secondNumber));
+                }
+                else if (action == "/")
+                {
                     if (secondNumber == 0)
                         Console.WriteLine(0);
                     else
-                        Console.WriteLine(firstNumber + "/" + secondNumber + "=" + (firstNumber / secondNumber));
-                    break;
-                default:
-                    Console.WriteLine("Этого я пока не умею");
-                    break;
+                        Console.WriteLine(firstNumber + action + secondNumber + "=" + (firstNumber / secondNumber));
+                }
+                else 
+                {
+                    Console.WriteLine("Этого я пока не умею делать!");
+                }
+                */
+                Console.ReadLine();
             }
             
-            /*
-            if (action == "+")
-            {
-                Console.WriteLine(firstNumber + action + secondNumber + "=" + (firstNumber + secondNumber));
-            }
-            else if (action == "-")
-            {
-                Console.WriteLine(firstNumber + action + secondNumber + "=" + (firstNumber - secondNumber));
-            }
-            else if (action == "*")
-            {
-                Console.WriteLine(firstNumber + action + secondNumber + "=" + (firstNumber * secondNumber));
-            }
-            else if (action == "/")
-            {
-                if (secondNumber == 0)
-                    Console.WriteLine(0);
-                else
-                    Console.WriteLine(firstNumber + action + secondNumber + "=" + (firstNumber / secondNumber));
-            }
-            else 
-            {
-                Console.WriteLine("Этого я пока не умею делать!");
-            }
-            */
-            Console.ReadLine();
         }
     }
 }
