bb574d06e8e2abcfe3f215be45065ac879db1a32
Bulat Sharipov <trece.espada@gmail.com>
2020-11-20 01:29:59 +0500

    Update Program.cs

--- SimpleCode/CycleFor/Program.cs
+++ SimpleCode/CycleFor/Program.cs
@@ -9,18 +9,34 @@ namespace CycleFor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введдите высоту прямоугольника");
+            /*Console.WriteLine("Введдите высоту прямоугольника");
             int height = int.Parse(Console.ReadLine());
             Console.WriteLine("Введите длину прямоугольника");
             int width = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= height; i++)
+            */
+            for (int i = 0; i <= 10; i++)
             {
                 
-                for (int j = 0; j <= width; j++)
+                for (int j = 1; j <= i; j++)
                 {
                     Console.Write("#");
                 }
                 
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("ghbdtn");
+
+            for (int i = 10; i <= 10; i--)
+            {
+
+                for (int j = 10; j < i; j--)
+                {
+                    Console.Write("#");
+                }
+
                 Console.WriteLine();
             }
         }
