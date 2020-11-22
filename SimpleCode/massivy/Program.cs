e877605031f98f25a3c1b28ba816e90241a58ae0
Bulat Sharipov <trece.espada@gmail.com>
2020-11-22 23:18:27 +0500

    Решил задачи

--- SimpleCode/massivy/Program.cs
+++ SimpleCode/massivy/Program.cs
@@ -14,17 +14,37 @@ namespace massivy
             Console.WriteLine(myArray.Length);
             */
             int[] myArray = new int [4];
+            //заполнение массива
             Console.WriteLine("введите 4 элемента массива");
             for (int i = 0; i < 4; i++)
             {
                 myArray[i] = int.Parse(Console.ReadLine());
             }
-            
+            //вывод массива в обратном порядке
             for (int i = myArray.Length - 1; i >= 0; i--)
             {
                 Console.WriteLine(myArray[i]);
             }
-            
+            //сумма четных чисел
+            int sumCV = 0;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                
+                if (myArray[i] % 2 == 0) 
+                {
+                    sumCV = sumCV + myArray[i];
+                }
+            }
+            //наименьшее число
+            int minNumber = myArray[0];
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                minNumber = minNumber < myArray[i] ? minNumber : myArray[i];
+            }
+
+            Console.WriteLine("наименьшее число в массиве = " + minNumber);
+
+            Console.WriteLine("сумма четных чисел = " + sumCV);
          }
     }
 }
