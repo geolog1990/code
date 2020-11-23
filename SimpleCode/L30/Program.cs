90b3d32b0a53a793039714d0ecdd052186b5cc49
Bulat Sharipov <trece.espada@gmail.com>
2020-11-24 01:02:03 +0500

    create L30

---
+++ SimpleCode/L30/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace L30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] myArray;
+            myArray = new int[3, 5];
+            Console.WriteLine(myArray[0,2]); 
+        }
+    }
+}
