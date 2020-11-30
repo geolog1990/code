a3565e941d9779dc1e522ca2d6635223930849fd
Bulat Sharipov <trece.espada@gmail.com>
2020-12-01 00:47:11 +0500

    create new project

---
+++ SimpleCode/L35/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace L35
+{
+    class Program
+    {
+        static int Sum(int a, int b) 
+        {
+            int result = a + b;
+            return result;
+        }
+
+        static void PrintResult(int result) 
+        {
+            Console.WriteLine("Результат сложения: " + result);
+        }
+
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            c = Sum(a, b);                       
+                
+            PrintResult(c);
+        }
+    }
+}
