7e14c0176397806be4abd9d10c820cb8f8e0bb44
Bulat Sharipov <trece.espada@gmail.com>
2020-12-06 03:44:15 +0500

    ghbf

--- SimpleCode/L35/Program.cs
+++ SimpleCode/L35/Program.cs
@@ -15,15 +15,35 @@ namespace L35
             Console.WriteLine("Результат сложения: " + result);
         }
 
+        static void PrintManyTimes(int a, string sim) 
+        {
+            for (int i = 0; i <= a; i++)
+            {
+                Console.Write(sim);
+            }
+        }
+
         static void Main(string[] args)
         {
-            int a, b, c;
+            int a;
+            string s;
+            Console.WriteLine("Введите символ");
+            s = Console.ReadLine();
+            Console.WriteLine("Введите количество");
+            a = int.Parse(Console.ReadLine());
+
+            PrintManyTimes(a, s);
+
+            /*
             a = int.Parse(Console.ReadLine());
             b = int.Parse(Console.ReadLine());
 
             c = Sum(a, b);                       
                 
             PrintResult(c);
+            */
+
+
         }
     }
 }
