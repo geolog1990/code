7b3e2a0301b0f658c83593fbaaee020eb7a6f89b
Bulat Sharipov <trece.espada@gmail.com>
2020-12-07 01:07:23 +0500

    Update Program.cs

--- SimpleCode/L41/Program.cs
+++ SimpleCode/L41/Program.cs
@@ -4,9 +4,17 @@ namespace L41
 {
     class Program
     {
+        static int[] GetArray()
+        {
+            int[] myArray = null;
+            return myArray;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int [] myArray = GetArray();
+            myArray ??= new int[0];
+            Console.WriteLine("количество элементов в массиве " + myArray.Length);
         }
     }
 }
