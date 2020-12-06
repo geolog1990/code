22c992ef8cca5776ee262c62a93c6606a50e96b4
Bulat Sharipov <trece.espada@gmail.com>
2020-12-06 13:53:49 +0500

    new

---
+++ SimpleCode/L40/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace L40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+            /*if (str == null)
+            {
+                Console.WriteLine("нет данных");
+            }
+            else
+            {
+                Console.WriteLine(str);
+            }*/
+            Console.WriteLine(str ?? "нет данных");
+
+        }
+    }
+}
