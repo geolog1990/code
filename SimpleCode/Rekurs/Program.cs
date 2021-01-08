e85cad14a50d13ac5614ecc4c46c95781081a0f0
Bulat Sharipov <trece.espada@gmail.com>
2021-01-08 18:54:44 +0500

    1

---
+++ SimpleCode/Rekurs/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Rekurs
+{
+    class Program
+    {
+
+        class Item 
+        {
+            public int Value { get; set; }
+            public Item Child { get; set; }
+        }
+
+        static Item InitItem() 
+        {
+            return new Item()
+            {
+                Value = 5,
+                Child = new Item()
+                {
+                    Value = 10,
+                    Child = new Item()
+                    {
+                        Value = 2
+                    }
+                }
+
+            }; 
+        }
+
+        static void Print(Item item) 
+        {
+            if (item != null) 
+            {
+                Console.WriteLine(item.Value);
+                Print(item.Child);
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Item item = InitItem();
+            // Print(item); // Вывод с помощью рекурсии
+            for (Item i = item; i != null; i = i.Child) // Вывод без рекурсии
+            {
+                Console.WriteLine(i.Value);
+            }
+         }
+    }
+}
