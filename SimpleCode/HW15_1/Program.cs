ad4f7b4270d110f0e3471dbcee95cbc8d3372c96
Bulat Sharipov <trece.espada@gmail.com>
2021-01-09 15:52:26 +0500

    работа с массивом с помощью рекурсии

---
+++ SimpleCode/HW15_1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace HW15_1 
+
+    // Вывод массива на консоль с помощью рекурсии
+    // Вывод суммы элементов массива с помощью рекурсии
+    // Вывод суммы цифр числа
+{
+    class Program
+    {
+        static void PrintArray(int[] MyArray, int i = 0) // Вывод массива
+        {
+            
+            if (i < MyArray.Length)
+            {
+                Console.WriteLine(MyArray[i]);
+                PrintArray(MyArray, i + 1);
+            }
+        }
+
+        static int SumArray(int[] MyArray, int i = 0) // Сумма элементов массива
+        {            
+            if (i >= MyArray.Length)
+                return 0;
+            int result = SumArray(MyArray, i + 1);
+            return MyArray[i] + result; 
+                
+        }
+
+        static int SumNumbersOfArray(int value) 
+        {
+            if (value / 10 == 0)
+                return value;
+            int sum = (value % 10) + SumNumbersOfArray(value / 10);
+            return sum;
+
+        }
+        
+        static void Main(string[] args)
+        {
+            //int[] array = { 1, 2, 3, 4, 5 };
+            //printarray(array);
+            //int result = sumarray(array);
+            
+            int myValue = 560; 
+            int sum = SumNumbersOfArray(myValue);
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ SimpleCode/OOP/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    enum Color
+    {
+        Red,
+        Green,
+        Grey,
+        Black
+    }
+    
+    class Point
+    {
+        public int x;
+        public int y;
+        public Color color;
+    }
+
+    class Student
+    {
+        public Guid id;
+        public string firstName;
+        public string lastName;
+        public string middleName;
+        public int age;
+        public string group;
+    }
+    
+    class Program
+    {
+        static Student GetStudent() 
+        {
+            Student student = new Student();
+
+            student.firstName = "Мартин";
+            student.lastName = "Лютер";
+            student.middleName = "Кинг";
+            student.id = Guid.NewGuid();
+            student.age = 19;
+            student.group = "ЙЦУКЕН-1";
+
+            return student;
+        }
+
+        static void Print(Student student) 
+        {
+            Console.WriteLine("Информация о студенте:");
+            Console.WriteLine($"Id: {student.id}"); ;
+            Console.WriteLine($"Фамилия: {student.lastName}");
+            Console.WriteLine($"Имя: {student.firstName}");
+            Console.WriteLine($"Отчество: {student.middleName}");
+            Console.WriteLine($"Возраст: {student.age}");
+            Console.WriteLine($"Группа: {student.group}");
+        }
+        
+        static void Main(string[] args)
+        {
+            //Point p = new Point();
+            //p.x = 4;
+            //p.y = 2;
+            //p.color = Color.Green;
+
+            //Point p2 = new Point
+            //{
+            //    x = 2,
+            //    y = 6,
+            //    color = Color.Grey
+            //};
+
+            var firstStudent = GetStudent();
+
+            Print(firstStudent);
+        }
+    }
+}
